09efd0ad6af449de678507e48556317b9caa73aa
walshcoding <139472932+walshcoding@users.noreply.github.com>
2023-07-13 09:40:42 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://walshcoding.github.io/bats-vs-princesses/](https://walshcoding.github.io/bats-vs-princesses/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/walshcoding/bats-vs-princesses** and import
+
+## Edit this project ![Build status badge](https://github.com/walshcoding/bats-vs-princesses/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/walshcoding/bats-vs-princesses** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/walshcoding/bats-vs-princesses/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,177 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "ENEMY":return img`
+. . f f f . . . . . . . . f f f 
+. f f 2 c . . . . . . f c b b c 
+f f 2 c . . . . . . f c b b c . 
+f 2 f c . . . . . . f b c 2 2 . 
+f f f c c . c c . f c b b c 2 . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c 2 b c . 
+. c 1 b b b 1 b c b b c 2 c . . 
+. c 2 b b b 2 b b c c 2 2 . . . 
+c b b b b b b b b b c 2 . . . . 
+c b 1 f f 1 c b b b b f . . . . 
+f f 1 f f 1 f b b b b f 2 . . . 
+f f 2 2 2 2 f b b b b f 2 2 . . 
+. f 2 2 2 2 b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`;
+            case "image4":
+            case "food":return img`
+. . . . . . . . . . . 6 6 6 6 6 
+. . . . . . . . . 6 6 7 7 7 7 8 
+. . . . . . 8 8 8 7 7 8 8 6 8 8 
+. . e e e e c 6 6 8 8 . 8 7 8 . 
+. e 2 5 4 2 e c 8 . . . 6 7 8 . 
+e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+e 2 e e 2 2 2 2 e e e e c 6 8 . 
+c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+. c 2 e e e 2 e 2 4 2 2 2 2 c . 
+. . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+. . . e c c e c 2 2 2 2 2 2 2 e 
+. . . . . . . c 2 e e 2 2 e 2 c 
+. . . . . . . c e e e e e e 2 c 
+. . . . . . . . c e 2 2 2 2 c . 
+. . . . . . . . . c c c c c . . 
+`;
+            case "image3":
+            case "BAD BAT":return img`
+. . . . . 2 . 2 . . . . . . . . 
+. . . . f 2 2 2 f f . . . . . . 
+. . . f 1 2 c 2 6 6 f . . . . . 
+. . f 1 6 6 6 6 6 1 6 f . . . . 
+. . f 6 6 f f f f 6 1 f . . . . 
+. . f 6 f d d d d f 6 f . f f f 
+. f 6 f d d d d d d f 6 f f 1 f 
+. f 6 f d d d d d d f 6 f 1 f f 
+. f 6 6 f d d d e f 6 f 1 f f . 
+f 6 6 f 2 f f f f e e 1 f f . . 
+. f f 2 2 c 2 2 c d e e f . . . 
+. . f d f 2 2 c 2 d d e f . . . 
+. . f d f 2 2 2 e 2 f . e . . . 
+. . f f 2 2 c 2 c 2 2 f . . . . 
+. . . f f f f f f f f f . . . . 
+. . . . . . . . f f . . . . . . 
+`;
+            case "image7":
+            case "BOSS":return img`
+. . . . . c . c . . . . . . . . 
+. . . . f c c c f f . . . . . . 
+. . . f 1 c 1 c 1 c f 2 . . . . 
+. . f 1 b b b b b 1 c 2 2 2 . . 
+. . f b b f f f f b 2 2 2 2 2 . 
+. . f b f d d d d f b f 2 2 2 . 
+. f b f d f 2 2 f d f e f 2 2 2 
+. f b f d d d d d d e b f . 2 2 
+. f b b f d d d d e b b f . . 2 
+f b b f 6 f f f e 6 f b b f . . 
+. f f d 6 c 6 e c 6 d f f . . . 
+. . f d d d e c 6 f d d f . . . 
+. . f f d e d 6 6 6 f f . . . . 
+. . f 6 e c 6 6 c 6 6 f . . . . 
+. . f f f f f f f f f f . . . . 
+. . . . f f . . f f . . . . . . 
+`;
+            case "image1":
+            case "Bat boy 1":return img`
+. . f f f . . . . . . . . . . . 
+f f f c c . . . . . . . . f f f 
+f f c c . . c c . . . f c b b c 
+f f c 3 c c 3 c c f f b b b c . 
+f f b 3 b c 3 b c f b b c c c . 
+. c b b b b b b c f b c b c c . 
+. c f 1 b b f 1 c b b c b b c . 
+c b 1 1 b b 1 1 b b c c c b c . 
+c b b b b b b b b c c c c c . . 
+f b f b b b f b b b b f c . . . 
+f b 1 f f f 1 b b b b f c c . . 
+. f b b b b b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "bad":return img`
+. . . . . . b b b b . . . . . . 
+. . . . b b a a a a b b . . . . 
+. . . c b a a a a 1 1 b c . . . 
+. . c b a a a a a 1 1 1 b c . . 
+. c c 1 1 1 a a a a 1 1 a c c . 
+c c d 1 1 1 a a a a a a a b c c 
+c b d d 1 a a a a a 1 1 1 b b c 
+c b b b a a 1 1 a a 1 1 d d b c 
+c b b b b d d 1 1 a b d d d b c 
+. c b b b b d d b b b b b b c . 
+. . c c b b b b b b b b c c . . 
+. . . . c c c c c c c c . . . . 
+. . . . . . b 1 1 b . . . . . . 
+. . . . . . b 1 1 b b . . . . . 
+. . . . . b b d 1 1 b . . . . . 
+. . . . . b d d 1 1 b . . . . . 
+`;
+            case "image5":
+            case "good":return img`
+. . . . . . b b b b . . . . . . 
+. . . . b b 3 3 3 3 b b . . . . 
+. . . c b 3 3 3 3 1 1 b c . . . 
+. . c b 3 3 3 3 3 1 1 1 b c . . 
+. c c 1 1 1 3 3 3 3 1 1 3 c c . 
+c c d 1 1 1 3 3 3 3 3 3 3 b c c 
+c b d d 1 3 3 3 3 3 1 1 1 b b c 
+c b b b 3 3 1 1 3 3 1 1 d d b c 
+c b b b b d d 1 1 3 b d d d b c 
+. c b b b b d d b b b b b b c . 
+. . c c b b b b b b b b c c . . 
+. . . . c c c c c c c c . . . . 
+. . . . . . b 1 1 b . . . . . . 
+. . . . . . b 1 1 b b . . . . . 
+. . . . . b b d 1 1 b . . . . . 
+. . . . . b d d 1 1 b . . . . . 
+`;
+            case "image8":
+            case "kkkk":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 1 2 . . . . . . 
+. . . . . . . 2 1 2 . . . . . . 
+. . . . . . . 2 1 2 . . . . . . 
+. . . . . . . 3 1 3 . . . . . . 
+. . . . . . 2 3 1 3 2 . . . . . 
+. . . . . . 2 1 1 1 2 . . . . . 
+. . . . . . 2 1 1 1 3 . . . . . 
+. . . . . . 3 1 1 1 3 . . . . . 
+. . . . . . 3 1 1 1 3 . . . . . 
+. . . . . . 3 1 1 1 3 . . . . . 
+. . . . . . 2 3 1 3 2 . . . . . 
+. . . . . . . 2 2 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
